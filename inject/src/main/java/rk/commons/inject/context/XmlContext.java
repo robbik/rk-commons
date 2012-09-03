2b7dc1f8693f4851e69d651ce18e7c6477f1ece5
Robbi Kurniawan <robbi.kurniawan@gmail.com>
2012-09-03 14:34:57 +0700

    Added new tag: require

--- inject/src/main/java/rk/commons/inject/context/XmlContext.java
+++ inject/src/main/java/rk/commons/inject/context/XmlContext.java
@@ -25,136 +25,159 @@ import rk.commons.logging.LoggerFactory;
 
 public class XmlContext {
 
-	private static final Logger log = LoggerFactory.getLogger(XmlContext.class);
-	
-	private ResourceLoader resourceLoader;
+    private static final Logger log = LoggerFactory.getLogger(XmlContext.class);
+    
+    private ResourceLoader resourceLoader;
 
-	private NamespaceHandlerResolver handlerResolver;
+    private NamespaceHandlerResolver handlerResolver;
 
-	private NamespaceSchemaResolver schemaResolver;
+    private NamespaceSchemaResolver schemaResolver;
 
-	protected ObjectFactory objectFactory;
+    protected ObjectFactory objectFactory;
 
-	protected ObjectDefinitionRegistry objectDefinitionRegistry;
-	
-	protected String xmlDefaultNamespace;
+    protected ObjectDefinitionRegistry objectDefinitionRegistry;
+    
+    protected String xmlDefaultNamespace;
 
-	private String[] locations;
+    private String[] locations;
 
-	public void setResourceLoader(ResourceLoader resourceLoader) {
-		this.resourceLoader = resourceLoader;
-	}
-	
-	public void setNamespaceHandlerPath(String path) {
-		handlerResolver = new NamespaceHandlerResolver(resourceLoader, path);
-	}
-	
-	public void setNamespaceSchemaPath(String path) {
-		schemaResolver = new NamespaceSchemaResolver(resourceLoader, path);
-	}
-	
-	public void setObjectFactory(ObjectFactory objectFactory) {
-		this.objectFactory = objectFactory;  
-	}
-	
-	public void setObjectDefinitionRegistry(ObjectDefinitionRegistry objectDefinitionRegistry) {
-		this.objectDefinitionRegistry = objectDefinitionRegistry;
-	}
-	
-	public void setXmlDefaultNamespace(String xmlDefaultNamespace) {
-		this.xmlDefaultNamespace = xmlDefaultNamespace;
-	}
-	
-	public void setLocations(String... locations) {
-		this.locations = locations;
-	}
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+    
+    public void setNamespaceHandlerPath(String path) {
+        handlerResolver = new NamespaceHandlerResolver(resourceLoader, path);
+    }
+    
+    public void setNamespaceSchemaPath(String path) {
+        schemaResolver = new NamespaceSchemaResolver(resourceLoader, path);
+    }
+    
+    public void setObjectFactory(ObjectFactory objectFactory) {
+        this.objectFactory = objectFactory;  
+    }
+    
+    public void setObjectDefinitionRegistry(ObjectDefinitionRegistry objectDefinitionRegistry) {
+        this.objectDefinitionRegistry = objectDefinitionRegistry;
+    }
+    
+    public void setXmlDefaultNamespace(String xmlDefaultNamespace) {
+        this.xmlDefaultNamespace = xmlDefaultNamespace;
+    }
+    
+    public void setLocations(String... locations) {
+        this.locations = locations;
+    }
 
-	protected void loadObjectDefinitions(URL url, Set<URL> importedURLs) {
-		if (!importedURLs.add(url)) {
-			// already loaded, skip it!
-			return;
-		}
-		
-		ObjectDefinitionParserDelegate delegate = new ObjectDefinitionParserDelegate(resourceLoader);
-		delegate.setObjectDefinitionRegistry(objectDefinitionRegistry);
-		delegate.setNamespaceHandlerResolver(handlerResolver);
-		delegate.setObjectNamePrefix(null);
+    protected void loadObjectDefinitions(URL url, Set<URL> importedURLs) {
+        if (!importedURLs.add(url)) {
+            // already loaded, skip it!
+            return;
+        }
+        
+        ObjectDefinitionParserDelegate delegate = new ObjectDefinitionParserDelegate(resourceLoader);
+        delegate.setObjectDefinitionRegistry(objectDefinitionRegistry);
+        delegate.setNamespaceHandlerResolver(handlerResolver);
+        delegate.setObjectNamePrefix(null);
 
-		XmlObjectDefinitionReader reader = new XmlObjectDefinitionReader(url, schemaResolver);
+        XmlObjectDefinitionReader reader = new XmlObjectDefinitionReader(url, schemaResolver);
 
-		Element rootElement = null;
+        Element rootElement = null;
 
-		try {
-			rootElement = reader.parse();
-		} catch (SAXException e) {
-			if (e instanceof SAXParseException) {
-				SAXParseException pex = (SAXParseException) e;
-				
-				throw new RuntimeException(
-						pex.getMessage() + " (" + url + ":"
-								+ pex.getLineNumber() + ":"
-								+ pex.getColumnNumber() + ")", e.getCause());
-			} else {
-				throw new RuntimeException(e.getMessage() + "(" + url + ")", e);
-			}
-		} catch (IOException e) {
-			log.error("unable to parse " + url, e);
-		}
+        try {
+            rootElement = reader.parse();
+        } catch (SAXException e) {
+            if (e instanceof SAXParseException) {
+                SAXParseException pex = (SAXParseException) e;
+                
+                throw new RuntimeException(
+                        pex.getMessage() + " (" + url + ":"
+                                + pex.getLineNumber() + ":"
+                                + pex.getColumnNumber() + ")", e.getCause());
+            } else {
+                throw new RuntimeException(e.getMessage() + "(" + url + ")", e);
+            }
+        } catch (IOException e) {
+            log.error("unable to parse " + url, e);
+        }
 
-		if (rootElement == null) {
-			return;
-		}
+        if (rootElement == null) {
+            return;
+        }
 
-		NodeList nodes = rootElement.getChildNodes();
+        NodeList nodes = rootElement.getChildNodes();
 
-		for (int i = 0, n = nodes.getLength(); i < n; ++i) {
-			Node node = nodes.item(i);
+        for (int i = 0, n = nodes.getLength(); i < n; ++i) {
+            Node node = nodes.item(i);
 
-			if (node instanceof Element) {
-				Element element = (Element) node;
+            if (node instanceof Element) {
+                Element element = (Element) node;
 
-				if (xmlDefaultNamespace.equals(node.getNamespaceURI()) && "import".equals(node.getLocalName())) {
-					URL[] refURLs = resourceLoader.getURLs(element.getAttribute("url"));
+                if (xmlDefaultNamespace.equals(node.getNamespaceURI())) {
+                    if ("import".equals(node.getLocalName())) {
+                        URL[] refURLs = resourceLoader.getURLs(element.getAttribute("url"));
 
-					for (int j = 0, m = refURLs.length; j < m; ++j) {
-						loadObjectDefinitions(refURLs[j], importedURLs);
-					}
-				} else {
-					delegate.parse((Element) node);
-				}
-			}
-		}
-	}
+                        for (int j = 0, m = refURLs.length; j < m; ++j) {
+                            loadObjectDefinitions(refURLs[j], importedURLs);
+                        }
+                    } else if ("require".equals(node.getLocalName())) {
+                        URL[] refURLs = resourceLoader.getURLs(element.getAttribute("url"));
 
-	public void refresh(boolean lazy) {
-		Set<URL> importedURLs = new HashSet<URL>();
-		
-		for (int i = 0, n = locations.length; i < n; ++i) {
-			URL[] docURLs = resourceLoader.getURLs(locations[i]);
-			
-			for (int j = 0; j < docURLs.length; ++j) {
-				loadObjectDefinitions(docURLs[j], importedURLs);
-			}
-		}
+                        if (refURLs.length == 0) {
+                            throw new RuntimeException("no required resources " +
+                                    element.getAttribute("url") + " by " + url);
+                        }
 
-		if (!lazy) {
-			Set<String> objectQNames = objectDefinitionRegistry.getObjectNames();
-	
-			for (String objectQName : objectQNames) {
-				ObjectDefinition def;
-				
-				try {
-					def = objectDefinitionRegistry.getObjectDefinition(objectQName);
-				} catch (ObjectNotFoundException e) {
-					continue;
-				}
-				
-				objectFactory.createObject(def);
-			}
-		}
-	}
+                        for (int j = 0, m = refURLs.length; j < m; ++j) {
+                            loadObjectDefinitions(refURLs[j], importedURLs);
+                        }
+                    } else {
+                        delegate.parse((Element) node);
+                    }
+                } else {
+                    delegate.parse((Element) node);
+                }
+            }
+        }
+    }
 
-	public void refresh(String location) {
-		loadObjectDefinitions(resourceLoader.getURL(location), new HashSet<URL>());
-	}
+    public Set<URL> refresh(boolean lazy) {
+        Set<URL> importedURLs = new HashSet<URL>();
+        
+        for (int i = 0, n = locations.length; i < n; ++i) {
+            URL[] docURLs = resourceLoader.getURLs(locations[i]);
+            
+            for (int j = 0; j < docURLs.length; ++j) {
+                loadObjectDefinitions(docURLs[j], importedURLs);
+            }
+        }
+
+        if (!lazy) {
+            Set<String> objectQNames = objectDefinitionRegistry.getObjectNames();
+    
+            for (String objectQName : objectQNames) {
+                ObjectDefinition def;
+                
+                try {
+                    def = objectDefinitionRegistry.getObjectDefinition(objectQName);
+                } catch (ObjectNotFoundException e) {
+                    continue;
+                }
+                
+                objectFactory.createObject(def);
+            }
+        }
+
+        return importedURLs;
+    }
+
+    public URL refresh(String location) {
+        URL docURL = resourceLoader.getURL(location);
+
+        if (docURL != null) {
+            loadObjectDefinitions(docURL, new HashSet<URL>());
+        }
+
+        return docURL;
+    }
 }
